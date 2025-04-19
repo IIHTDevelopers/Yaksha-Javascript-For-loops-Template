58d4598c23c333efa85f91950fa9443b6ee71b7d
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-04-19 09:01:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// Using a for loop to count from 1 to 5
+for (var i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+// Using a for loop to iterate over an array
+var arr = ['apple', 'banana', 'cherry'];
+for (var i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
